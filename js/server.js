1ef03f727c81a2c5417fdeb4dacad93a6bbbeb81
saigaikinkyuu <125773463+saigaikinkyuu@users.noreply.github.com>
2025-10-16 11:53:58 +0900

    fix: serverエラーの軽微な修正

--- js/server.js
+++ js/server.js
@@ -1,3 +1,17 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDzslg1WbmtYBNFtR3BrrHVvXYTeqanDr8",
+    authDomain: "home-recipe-be23b.firebaseapp.com",
+    projectId: "home-recipe-be23b",
+    storageBucket: "home-recipe-be23b.firebasestorage.app",
+    messagingSenderId: "801879261323",
+    appId: "1:801879261323:web:0d2f9552d1058ee99d948e",
+    measurementId: "G-Y12V9FEK27"
+};
+
+const app = firebase.initializeApp(firebaseConfig);
+const auth = app.auth();
+const db = app.firestore();
+
 var isRun = false;
 auth.onAuthStateChanged(async user => {
     if (user) {
